c09900b88af09aa4db1adb2a0239e6bcb797275a
puiminh <volmicrolab@gmail.com>
2023-07-16 18:10:26 +0700

    add record (execl file)

---
+++ src/main/java/com/project/chatbot/Data.java
@@ -0,0 +1,26 @@
+package com.project.chatbot;
+public class Data{
+    public String data = "Tôi muốn nâng cao từ vựng về phương tiện giao thông. " +
+            "Cho tôi học từ vựng về công nghệ thông tin. " +
+            "Tôi muốn tìm hiểu từ vựng về thể thao. " +
+            "Hãy giúp tôi học từ vựng về du lịch. " +
+            "Tôi cần học từ vựng về kỹ năng làm việc. " +
+            "Tôi muốn tìm hiểu từ vựng về công việc văn phòng. " +
+            "Hãy giúp tôi học từ vựng về âm nhạc. " +
+            "Tôi muốn nắm vững từ vựng về y tế. " +
+            "Tôi cần học từ vựng về chủ đề khoa học. " +
+            "Hãy tạo cho tôi một bộ từ vựng liên quan đến trái cây. " +
+            "Tôi muốn học từ vựng về môi trường. " +
+            "Tôi cần nắm vững từ vựng về công nghiệp. " +
+            "Cho tôi học từ vựng về địa lý. " +
+            "Tôi muốn tìm hiểu từ vựng về phim ảnh. " +
+            "Hãy giúp tôi học từ vựng về thời tiết. " +
+            "Tôi muốn nâng cao từ vựng về lịch sử. " +
+            "Tôi cần học từ vựng về nghệ thuật. " +
+            "Tôi muốn tìm hiểu từ vựng về trò chơi điện tử. " +
+            "Hãy giúp tôi học từ vựng về chính trị. " +
+            "Tôi muốn nắm vững từ vựng về kinh tế. ";
+    public String getData(){
+        return this.data;
+    }
+}
--- src/main/java/com/project/chatbot/VnCoreNLPExample.java
+++ src/main/java/com/project/chatbot/VnCoreNLPExample.java
@@ -1,11 +1,18 @@
 package com.project.chatbot;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import vn.pipeline.Annotation;
 import vn.pipeline.Sentence;
 import vn.pipeline.VnCoreNLP;
+import vn.pipeline.Word;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.List;
 
 public class VnCoreNLPExample {
     public static void main(String[] args) throws IOException {
@@ -13,13 +20,59 @@ public class VnCoreNLPExample {
         // "wseg", "pos", "ner", and "parse" refer to as word segmentation, POS tagging, NER and dependency parsing, respectively. 
         String[] annotators = {"wseg", "pos", "ner", "parse"}; 
         VnCoreNLP pipeline = new VnCoreNLP(annotators); 
-    
-        String str = "Ông Nguyễn Khắc Chúc  đang làm việc tại Đại học Quốc gia Hà Nội. "
-                    + "Bà Lan, vợ ông Chúc, cũng làm việc tại đây."; 
+
+        Data data = new Data();
+        String str = data.getData();
         Annotation annotation = new Annotation(str); 
         pipeline.annotate(annotation); 
-        
+        List<Word> words = annotation.getWords();
+        List<String> tokens = annotation.getTokens();
+        String wordSegmentedText = annotation.getWordSegmentedText();
+        String wordSegmentedTaggedText = annotation.getWordSegmentedTaggedText();
         System.out.println(annotation.toString());
+        System.out.println(words.toString());
+
+        // Tạo một workbook mới
+        Workbook workbook = new XSSFWorkbook();
+        // Tạo một trang tính mới trong workbook
+        Sheet sheet = workbook.createSheet("Words");
+
+        // Tạo tiêu đề cho các cột
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("Index");
+        headerRow.createCell(1).setCellValue("Form");
+        headerRow.createCell(2).setCellValue("POS Tag");
+        headerRow.createCell(3).setCellValue("NER Label");
+        headerRow.createCell(4).setCellValue("Head");
+        headerRow.createCell(5).setCellValue("Dep Label");
+
+        // Ghi dữ liệu từ danh sách words vào các hàng trong sheet
+        int rowIndex = 1;
+        for (Word word : words) {
+            Row dataRow = sheet.createRow(rowIndex);
+            dataRow.createCell(0).setCellValue(word.getIndex());
+            dataRow.createCell(1).setCellValue(word.getForm());
+            dataRow.createCell(2).setCellValue(word.getPosTag());
+            dataRow.createCell(3).setCellValue(word.getNerLabel());
+            dataRow.createCell(4).setCellValue(word.getHead());
+            dataRow.createCell(5).setCellValue(word.getDepLabel());
+            rowIndex++;
+        }
+
+        // Lưu workbook vào file
+        try (FileOutputStream fileOut = new FileOutputStream("output.xlsx")) {
+            workbook.write(fileOut);
+            System.out.println("File Excel đã được tạo thành công!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Đóng workbook
+        try {
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         // 1    Ông                 Nc  O       4   sub 
         // 2    Nguyễn_Khắc_Chúc    Np  B-PER   1   nmod
         // 3    đang                R   O       4   adv
@@ -31,7 +84,7 @@ public class VnCoreNLPExample {
 //        pipeline.printToFile(annotation, outputPrinter);
     
         // You can also get a single sentence to analyze individually 
-        Sentence firstSentence = annotation.getSentences().get(0);
-        System.out.println(firstSentence.toString());
+//        Sentence firstSentence = annotation.getSentences().get(0);
+//        System.out.println(firstSentence.toString());
     }
 }
