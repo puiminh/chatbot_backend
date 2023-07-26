92dfe5e273934d9df88e6f27bd2601b70ae6629a
puiminh <volmicrolab@gmail.com>
2023-07-26 13:21:07 +0700

    fix cors

--- src/main/java/com/project/chatbot/controller/MessageController.java
+++ src/main/java/com/project/chatbot/controller/MessageController.java
@@ -16,6 +16,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500", maxAge = 3600)
 @RestController
 public class MessageController {
     @Autowired
